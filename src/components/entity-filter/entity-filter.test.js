1f2c6ca423de3435096830193aa127cc193d43a0
matheusmr13 <matheusmr00@gmail.com>
2018-01-20 20:53:13 -0200

    Tests

--- src/components/entity-filter/entity-filter.test.js
+++ src/components/entity-filter/entity-filter.test.js
@@ -52,10 +52,22 @@ const filters = [{
 describe('EntityFilter component', () => {
 	it('should render properly', () => {
 		const entityFilter = shallow(<EntityFilter filters={filters} />);
+		expect(entityFilter.find(FieldFilter)).toHaveLength(5);
 		expect(entityFilter).toMatchSnapshot();
 	});
-	it('should has correct fields on created filter', () => {
-		const entityFilter = shallow(<EntityFilter filters={filters} />);
-		expect(entityFilter.find(FieldFilter)).toHaveLength(5);
+	it('should record all entity filter after change', () => {
+		let actualFilters;
+		const onChange = (newFilters) => {
+			actualFilters = newFilters;
+		};
+		const entityFilter = shallow(<EntityFilter filters={filters} onChange={onChange} />);
+		entityFilter.instance().onChange('offset', 1);
+		expect(actualFilters).toEqual({ offset: 1 });
+
+		entityFilter.instance().onChange('limit', 2);
+		expect(actualFilters).toEqual({ offset: 1, limit: 2 });
+
+		entityFilter.instance().onChange('timestamp', '2018-10-10T10:10:00');
+		expect(actualFilters).toEqual({ offset: 1, limit: 2, timestamp: '2018-10-10T10:10:00' });
 	});
 });
--- src/components/field-filter/__test__/unlimited-integer-filter.test.js
+++ src/components/field-filter/__test__/unlimited-integer-filter.test.js
@@ -2,12 +2,28 @@ import React from 'react';
 import UnlimitedIntegerFilter from 'components/field-filter/unlimited-integer-filter';
 import { shallow } from 'enzyme';
 
+const spec = {
+	name: 'Unimited integer field'
+};
 describe('UnlimitedIntegerFilter component', () => {
-	it('should render properly', () => {
-		const spec = {
-			name: 'Unimited integer field'
-		};
-		const unlimitedIntegerFilter = shallow(<UnlimitedIntegerFilter spec={spec} />);
+	it('should render initial state properly', () => {
+		const unlimitedIntegerFilter = shallow(<UnlimitedIntegerFilter spec={spec} onChange={() => {}} />);
+		expect(unlimitedIntegerFilter.find('button')).toHaveLength(1);
+		expect(unlimitedIntegerFilter).toMatchSnapshot();
+	});
+	it('should render properly after up click', () => {
+		const unlimitedIntegerFilter = shallow(<UnlimitedIntegerFilter spec={spec} onChange={() => {}} />);
+
+		unlimitedIntegerFilter.find('button').simulate('click');
+		expect(unlimitedIntegerFilter.find('button')).toHaveLength(2);
+		expect(unlimitedIntegerFilter).toMatchSnapshot();
+	});
+	it('should render properly after up and down click', () => {
+		const unlimitedIntegerFilter = shallow(<UnlimitedIntegerFilter spec={spec} onChange={() => {}} />);
+
+		unlimitedIntegerFilter.find('button').simulate('click');
+		unlimitedIntegerFilter.find('button').first().simulate('click');
+		expect(unlimitedIntegerFilter.find('button')).toHaveLength(1);
 		expect(unlimitedIntegerFilter).toMatchSnapshot();
 	});
 });
