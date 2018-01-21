afd8ea3f5f0b9b3f0f514c2d911500ec580e5355
matheusmr13 <matheusmr00@gmail.com>
2018-01-21 14:32:21 -0200

    Tests

--- src/services/playlist-filter/__test__/saga.test.js
+++ src/services/playlist-filter/__test__/saga.test.js
@@ -1,10 +1,14 @@
 import SagaTester from 'redux-saga-tester';
 import { Actions } from 'services/playlist-filter/module';
+import { Actions as PlaylistActions } from 'services/playlist/module';
 import PlaylistFilterSaga from 'services/playlist-filter/saga';
 import PlaylistFilterService from 'services/playlist-filter/service';
 import { reducers } from 'services/config/redux';
 
 PlaylistFilterService.fetchFilters = jest.fn(() => Array(2).fill({}));
+jest.mock('services/spotify/service', () => ({
+	fetchFeaturedPlaylists: () => Promise.resolve({ playlists: { items: Array(5).fill({}) } })
+}));
 
 describe('PlaylistFilterSaga', () => {
 	let sagaTester = null;
@@ -24,5 +28,13 @@ describe('PlaylistFilterSaga', () => {
 		const lastAction = sagaTester.getLatestCalledAction();
 		expect(lastAction.payload).toHaveLength(2);
 		expect(PlaylistFilterService.fetchFilters).toHaveBeenCalled();
+	});
+
+	it('should change playlist filter when filter changed', async () => {
+		sagaTester.dispatch(Actions.onFilterChange());
+		await sagaTester.waitFor(PlaylistActions.filterPlaylists.toString());
+
+		const lastAction = sagaTester.getLatestCalledAction();
+		expect(lastAction.payload).toBeFalsy();
 	});
 });
