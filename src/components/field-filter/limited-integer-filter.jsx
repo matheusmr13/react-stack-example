d47aa2d0520a33b0fa471c93ae8422ad416aa65e
matheusmr13 <matheusmr00@gmail.com>
2018-01-25 23:55:30 -0200

    Label

--- src/components/field-filter/limited-integer-filter.jsx
+++ src/components/field-filter/limited-integer-filter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import FilterPropType from 'services/playlist-filter/proptype';
 
@@ -6,18 +6,36 @@ import Slider from 'material-ui/Slider';
 
 import './limited-integer-filter.css';
 
-const LimitedIntegerFilter = ({ spec: { validation, name }, onChange }) => (
-	<div className="li-filter">
-		<div>{name}</div>
-		<Slider
-			defaultValue={(validation.min + validation.max) / 2}
-			min={validation.min}
-			max={validation.max}
-			step={1}
-			onChange={(a, value) => onChange(value)}
-		/>
-	</div>
-);
+class LimitedIntegerFilter extends Component {
+	constructor(props) {
+		super(props);
+		const { validation } = props.spec;
+		this.state = {
+			value: parseInt((validation.min + validation.max) / 2, 10)
+		};
+	}
+
+	onChange = (e, value) => {
+		this.setState({ value });
+		this.props.onChange(value);
+	}
+	render() {
+		const { spec: { validation, name } } = this.props;
+		const { value } = this.state;
+		return (
+			<div className="li-filter">
+				<div>{name}: {value}</div>
+				<Slider
+					value={value}
+					min={validation.min}
+					max={validation.max}
+					step={1}
+					onChange={this.onChange}
+				/>
+			</div>
+		);
+	}
+}
 
 LimitedIntegerFilter.defaultProps = {
 	onChange: () => {}
